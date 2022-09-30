b930e1e78330c38019b765e12aecc0f8dc3fbf23
JJASMINE22 <m13541280433@163.com>
2022-09-30 09:37:00 +0800

    yolo-tiny

--- train.py
+++ train.py
@@ -59,9 +59,11 @@ if __name__ == '__main__':
                     'acc': Yolo.train_acc / (i + 1) * 100},
                    cfg.ckpt_path + '\\Epoch{:0>3d}_train_loss{:.3f}_train_acc{:.3f}.pth.tar'.format(
                        epoch + 1, Yolo.train_loss / (i + 1), Yolo.train_acc / (i + 1) * 100))
+        
         Yolo.train_loss = 0
         Yolo.train_acc = 0
         Yolo.train_conf_acc = 0
+        Yolo.train_f1_score = 0
 
         for i in range(data_gen.get_val_len()):
             sources, targets = next(validate_gen)
@@ -80,3 +82,4 @@ if __name__ == '__main__':
         Yolo.val_loss = 0
         Yolo.val_acc = 0
         Yolo.val_conf_acc = 0
+        Yolo.val_f1_score = 0
